06dc3a77be883d22556f3c45dd71cf8aae337e73
riefqialviansyah <riefqi.alviansyah@gmail.com>
2024-05-23 19:15:08 +0800

    add welcome endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to my API",
+    createdBy: "Riefqi Alviansyah",
+    github: "https://github.com/riefqialviansyah",
+    readme: "https://github.com/riefqialviansyah/project-backend",
+  });
+});
+
 router.use("/portfolio", require("./portfolioRoute"));
 
 module.exports = router;
